eb240817756541e30a4a380bd9e92608e1e1b329
Jeremy Gagnier <jer.gagnier@gmail.com>
2026-06-21 15:02:39 -0700

    fmt

--- src/dlq.rs
+++ src/dlq.rs
@@ -1,8 +1,7 @@
 use serde::{Deserialize, Serialize, ser};
 
 #[derive(Clone, Serialize, Deserialize, Debug)]
-pub enum DLQData<T: ser::Serialize>
-{
+pub enum DLQData<T: ser::Serialize> {
   /// Message is used when the message can be deserialized.
   Message(T),
   /// Bytes is used when the message cannot be deserialized.
@@ -10,8 +9,7 @@ pub enum DLQData<T: ser::Serialize>
 }
 
 #[derive(Clone, Serialize, Deserialize, Debug)]
-pub struct DLQMessage<V: ser::Serialize>
-{
+pub struct DLQMessage<V: ser::Serialize> {
   /// error that caused the message to be published to the DLQ.
   pub error: String,
   /// value is the payload of the message that encountered the error during handling.
